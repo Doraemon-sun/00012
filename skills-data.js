9b565ae3b4314f341757142667e301b64559a998
Doraemon-sun <tungscript@gmail.com>
2025-11-09 15:06:38 +0700

    Update capabilities and add new skill category

--- skills-data.js
+++ skills-data.js
@@ -3,12 +3,18 @@ window.SKILLS = [
         id: 'Kỹ năng côt lõi',
         name: 'Kỹ năng cốt lõi',
         description: 'Các kỹ năng cốt lõi được tổng hợp lại trong quá trình học ở trong trường.',
-        capabilities: ['User Interview', 'Persona', 'Journey Mapping']
+        capabilities: ['Đo lường điện', 'Khí cụ điện', 'Kỹ thuật lắp đặt điện', 'Mạch điện', 'Điện tử cơ bản', 'Máy điện 1', 'Cung cấp diện', 'Điện tử công suất', 'Diều khiển điện khí nén', 'Plc cơ bản', 'Trang Bị điện']
     },
     {
         id: 'kỹ năng mền & khác',
         name: 'Kỹ năng mền & kỹ năng khác',
         description: 'Các kỹ năng mền  được tổng hợp và học hỏi trong quá trình làm việc để đưa ra.',
         capabilities: ['Hàn hồ quang cơ cản ( que hàn , mig)', 'Hàn cáp quang cơ bản', 'chuẩn(a) và chuẩn(b)', 'Kéo cáp ( đào hố kéo cáp , leo cột kéo cáp )', 'Dọc bản vẽ kỹ thuật cơ bản']
+    },
+     {
+        id: 'kỹ năng Khác',
+        name: 'Kỹ năng khác',
+        description: 'Các kỹ năng mền về máy tính .',
+        capabilities: ['Tin học cơ bản']
     }
 ];
