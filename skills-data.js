da66a445851911a5d17bd10b55a982440cd17432
Doraemon-sun <tungscript@gmail.com>
2025-11-09 18:32:20 +0700

    Update skills-data.js

--- skills-data.js
+++ skills-data.js
@@ -9,7 +9,7 @@ window.SKILLS = [
         id: 'kỹ năng mền & khác',
         name: 'Kỹ năng mền & kỹ năng khác',
         description: 'Các kỹ năng mền  được tổng hợp và học hỏi trong quá trình làm việc để đưa ra.',
-        capabilities: ['Hàn hồ quang cơ cản ( que hàn , mig)', 'Hàn cáp quang cơ bản', 'chuẩn(a) và chuẩn(b)', 'Kéo cáp ( đào hố kéo cáp , leo cột kéo cáp )', 'Dọc bản vẽ kỹ thuật cơ bản']
+        capabilities: ['Hàn hồ quang cơ cản ( que hàn , mig)', 'Hàn cáp quang cơ bản', 'chuẩn(a) và chuẩn(b)', 'Kéo cáp ( đào hố kéo cáp , leo cột kéo cáp )', 'Dọc bản vẽ kỹ thuật cơ bản', '5s']
     },
      {
         id: 'kỹ năng vi tính',
